68a589323eefeec33925ff70ad088c9845c8a8bf
Wilde Chu <wildechu23@gmail.com>
2021-10-15 21:19:52 -0400

    did the thing minus random

--- construction.c
+++ construction.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct profile {
 	char name[16];
@@ -8,14 +9,23 @@ struct profile {
 };
 
 void print_profile(struct profile *p) {
-	printf("%s has %d wins and %d losses.", p->name, p->wins,p->losses);
+	printf("%s has %d wins and %d losses.\n", p->name, p->wins,p->losses);
 }
 
-struct profile * make_profile(char *nm, int w, int l) {
-	
+struct profile * make_profile(char *nm, int sz, int w, int l) {
+	struct profile *n = malloc(sizeof(struct profile));
+
+  	strncpy(n->name, nm, sz);
+  	n->wins = w;
+	n->losses = l;
+
+  return n;
 }
 
 int main() {
+	struct profile *bob = make_profile("bob",3,3,5);
+	print_profile(bob);
 
+	free(bob);
 	return 0;
 }
